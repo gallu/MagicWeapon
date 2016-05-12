90a5d6c701c740d4667b711a969156a455565469
gallu <m.furusho@gmail.com>
2016-05-12 20:35:19 +0900

    openssl版一旦コミット。…所々ベタで書いてるので些か怪しい可能性あり…

---
+++ mw_crypt_openssl_T.php
@@ -0,0 +1,15 @@
+<?php
+
+require_once('mw_crypt_openssl.inc');
+require_once('debug_util.inc');
+
+$obj = new mw_crypt_openssl();
+$obj->set_cipher_rijndael_256();
+$obj->set_key_base('test');
+//
+$ciphertext = $obj->encrypt('あいうえお');
+var_dump($ciphertext);
+
+//
+$plaintext = $obj->decrypt_base64_decodde($ciphertext);
+var_dump($plaintext);
---
+++ mw_crypt_openssl_TT.php
@@ -0,0 +1,47 @@
+<?php
+// 対象になるクラスをinclude
+require_once('mw_crypt_openssl.inc');
+
+  //
+  $data = array(
+    'asoejr9873',
+    'あいうえおかきくけこ',
+    '漢字交じり',
+    '漢字iii交じり',
+  );
+  $cipher = array(
+    'rijndael_128',
+    'rijndael_192',
+    'rijndael_256',
+    'blowfish',
+  );
+  $mode = array (
+    'cbc',
+    'ecb',
+  );
+
+  // 等しい系テスト
+  foreach($data as $d) {
+    foreach($cipher as $c) {
+      foreach($mode as $m) {
+        //
+        $obj = new mw_crypt_openssl();
+        $s = "set_cipher_{$c}";
+        $obj->$s();
+        $s = "set_mode_{$m}";
+        $obj->$s();
+        $obj->set_key_base('test');
+        $crypttext = $obj->encrypt($d);
+        //
+        $plaintext = $obj->decrypt_base64_decodde($crypttext);
+
+        //
+        if($d === $plaintext) {
+          echo "ok\n";
+        } else {
+          echo "ng!!!!!!!!!!!!!!!!!!!!!\n";
+        }
+      }
+    }
+  }
+
